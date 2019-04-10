484d90936af472dddccec1b43f9ec286eda80449
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2019-04-10 20:12:55 +0200

    fqctory vs constructor function

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ function createCircles(radius) {
     
 }
 
-// Constructor Function
-
 const circle = createCircles(1);
-circle.draw();
+
+// Constructor Function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+const another = new Circle(1);
+
+another.draw();
