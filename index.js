5cc2d634cdeae0585d9e5476dcf63d91b47d0319
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2019-04-11 10:00:13 +0200

    enumerating in obj

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-console.log('Bonjour');
-
-// Factory Function
-function createCircles(radius) {
-    return {
-        radius,
-        draw: function() {
-            console.log('draw');
-        }
-    };
-    
-}
-
-const circle = createCircles(1);
-
 // Constructor Function
 function Circle(radius) {
+    // console.log('this', this);
     this.radius = radius;
     this.draw = function() {
         console.log('draw');
     }
 }
-const another = new Circle(1);
+const circle = new Circle(1);
 
-another.draw();
+// console.log('this', this);
+
+// enumerating all the members in a obj
+for (let key in circle) {
+    if (typeof circle[key] !== 'function') {
+        console.log(key, circle[key]);
+    }
+}
+
+// get all the keys in an obj (array) 
+const keys = Object.keys(circle);
+console.log(keys);
+
+// check the existence of a prop or method
+if ('radius' in circle) {
+console.log('Circle has a radius');    
+} 
